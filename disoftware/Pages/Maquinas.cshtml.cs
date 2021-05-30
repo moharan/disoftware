32bb1609c2f82a52cb7492db33cb3f2c4c0bf8c9
Rangi Becerra <rangibecerra@Rangis-MacBook-Pro.local>
2021-05-30 17:51:21 -0400

    added maquinas and usuarios pages

---
+++ disoftware/Pages/Maquinas.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace disoftware.Pages
+{
+    public class MaquinasModel : PageModel
+    {
+        private readonly ILogger<MaquinasModel> _logger;
+
+        public MaquinasModel(ILogger<MaquinasModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ disoftware/Pages/Usuarios.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace disoftware.Pages
+{
+    public class UsuariosModel : PageModel
+    {
+        private readonly ILogger<UsuariosModel> _logger;
+
+        public UsuariosModel(ILogger<UsuariosModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
